7c48da81ee922923f58c7e0d09c38eb27be1e3a5
mark-datascience <144593706+mark-datascience@users.noreply.github.com>
2024-05-29 13:52:35 +0800

    Update construction_spending.sql

--- construction_spending.sql
+++ construction_spending.sql
@@ -16,3 +16,88 @@ ADD COLUMN construction_type VARCHAR(200); -- added column 'sector' and 'constru
 
 UPDATE PBWSCONS -- change table name
 SET sector = 'Public', construction_type = 'Water Supply'; -- change sector and construction type
+
+/*   ---- Combining Public and Private tables
+
+CREATE TABLE total_public_cons AS
+(
+SELECT 
+	*
+FROM
+	`PBAMUSCONS`
+UNION ALL
+
+SELECT 
+	*
+FROM
+	`PBCADCONS`
+UNION ALL
+
+SELECT 
+	*
+FROM
+	`PBCOMCONS`
+UNION ALL
+
+SELECT 
+	*
+FROM
+	`PBEDUCONS`
+UNION ALL
+
+SELECT 
+	*
+FROM
+	`PBHLTHCONS`
+UNION ALL
+
+SELECT 
+	*
+FROM
+	`PBHWYCONS`
+UNION ALL
+
+SELECT 
+	*
+FROM
+	`PBOFCONS`
+UNION ALL
+
+SELECT 
+	*
+FROM
+	`PBPSCONS`
+UNION ALL
+
+SELECT 
+	*
+FROM
+	`PBPWRCONS`
+UNION ALL
+
+SELECT 
+	*
+FROM
+	`PBRESCONS`
+
+UNION ALL
+
+SELECT 
+	*
+FROM
+	`PBSWGCONS`
+UNION ALL
+
+SELECT 
+	*
+FROM
+	`PBTRANSCONS`
+UNION ALL
+
+SELECT 
+	*
+FROM
+	`PBWSCONS`
+);
+
+-- combining tables, total public construction
